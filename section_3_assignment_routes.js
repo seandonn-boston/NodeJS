c8894ad90b40069a5e1bd2986a6d9064fff84225
seandonn-boston <sean@seandonn.io>
2021-02-23 12:10:38 -0500

    cleanup

--- section_3_assignment_routes.js
+++ section_3_assignment_routes.js
@@ -1,20 +1,29 @@
 const requestHandler = (req, res) => {
   const { url, method } = req;
   if (url === "/") {
-    res.write("<html><title>Sec3 Asgmt Home</title></html>");
+    res.write("<html>");
+    res.write("<title>Sec3 Asgmt Home</title>");
+    res.write("</html>");
     res.write("<body>");
     res.write("<h1>Create a user</h1>");
-    res.write(
-      "<form action='/create-user' method='POST'><input type='text' name='username'><button type='submit'>Create User</button></form>"
-    );
+    res.write("<form action='/create-user' method='POST'>");
+    res.write("<input type='text' name='username'>");
+    res.write("<button type='submit'>Create User</button>");
+    res.write("</form>");
     res.write("</body>");
     return res.end();
   }
   if (url === "/users") {
-    res.write("<html><title>Sec3 Asgmt Users</title></html>");
-    res.write(
-      "<body><h1>Users</h1><ul><li>User 1</li><li>User 2</li></ul></body>"
-    );
+    res.write("<html>");
+    res.write("<title>Sec3 Asgmt Users</title>");
+    res.write("</html>");
+    res.write("<body>");
+    res.write("<h1>Users</h1>");
+    res.write("<ul>");
+    res.write("<li>User 1</li>");
+    res.write("<li>User 2</li>");
+    res.write("</ul>");
+    res.write("</body>");
     return res.end();
   }
   if (url === "/create-user" && method === "POST") {
